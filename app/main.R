574a8c26b9b187cc48ec93b199dbeb406c9a1aef
Pawan Rama Mali <prm@outlook.in>
2023-07-20 16:34:53 +0530

    Update main.R

--- app/main.R
+++ app/main.R
@@ -226,9 +226,6 @@ server <- function(id) {
         "ExposedPorts": {
           "3838/tcp": {}
         },
-        "Volumes": {
-          "/root/app/pyFolder/": "/root/app/pyFolder/"
-        },
         "HostConfig": {
           "PortBindings": {
             "3838/tcp": [
